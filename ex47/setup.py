22b58c8ece29802ef49c60b8c49e183aaf6b6047
Marcin Szarek <marcinshk@gmail.com>
2015-05-16 18:27:18 +0000

    Initial version of toy ex47 unit tests project.

---
+++ ex47/setup.py
@@ -0,0 +1,27 @@
+#! python
+#
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+"""
+    Make sure you read the documentation for distutils:
+        http://docs.python.org/distutils/setupscript.html.
+"""
+
+config = {
+          'description': 'LPtHW, Exc. 47',
+          'author': 'Marcin Szarek',
+          'url': 'https://github.com/marcinshk/Python-projects.git',
+          'download_url': 'https://github.com/marcinshk/Python-projects.git',
+          'author_email': 'marcinshk@gmail.com',
+          'version': '0.1',
+          'install_requires': ['nose2', 'setuptools', 'virtualenv'],
+          'packages': ['ex47-pkg'],
+          'scripts': [],
+          'name': 'ex47'
+    }
+
+setup(**config)
---
+++ ex47/tests/test_ex47.py
@@ -0,0 +1,12 @@
+
+from nose2.tools import *
+import ex47
+
+def setup():
+    print "SETUP!" 
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
